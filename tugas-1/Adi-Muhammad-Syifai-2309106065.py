51445a475cd990f1e5086898ada90c42da5da186
MathcaTi <tehmatcha28@gmail.com>
2023-09-30 09:45:44 +0800

    chore: prettier

--- tugas-1/Adi-Muhammad-Syifai-2309106065.py
+++ tugas-1/Adi-Muhammad-Syifai-2309106065.py
@@ -1,54 +1,57 @@
 import os
 
-print('| | _ | ||       ||   |    |       ||       ||  |_|  ||       |')
-print('| | _ | ||       ||   |    |       ||       ||  |_|  ||       |')
-print('| || || ||    ___||   |    |       ||   _   ||       ||    ___|')
-print('|       ||   |___ |   |    |       ||  | |  ||       ||   |___ ')
-print('|       ||    ___||   |___ |      _||  |_|  ||       ||    ___|')
-print('|   _   ||   |___ |       ||     |_ |       || ||_|| ||   |___ ')
-print('|__| |__||_______||_______||_______||_______||_|   |_||_______|')
-print('\nNama\t: Adi Muhammad Syifai')
-print('Kelas\t: Informatika B23')
-print('NIM\t: 2309106065\n')
+print("| | _ | ||       ||   |    |       ||       ||  |_|  ||       |")
+print("| | _ | ||       ||   |    |       ||       ||  |_|  ||       |")
+print("| || || ||    ___||   |    |       ||   _   ||       ||    ___|")
+print("|       ||   |___ |   |    |       ||  | |  ||       ||   |___ ")
+print("|       ||    ___||   |___ |      _||  |_|  ||       ||    ___|")
+print("|   _   ||   |___ |       ||     |_ |       || ||_|| ||   |___ ")
+print("|__| |__||_______||_______||_______||_______||_|   |_||_______|")
+print("\nNama\t: Adi Muhammad Syifai")
+print("Kelas\t: Informatika B23")
+print("NIM\t: 2309106065\n")
 
-retry = 'y'
+retry = "y"
+
 
 def getUserInput():
     while True:
         try:
-            x = int(input('Masukkan nilai Anda [0 - 100]: '))
+            x = int(input("Masukkan nilai Anda [0 - 100]: "))
             if x >= 0 and x <= 100:
                 return x
             else:
-                print('Nilai harus di dalam range 0 hingga 100!')
+                print("Nilai harus di dalam range 0 hingga 100!")
         except:
-            print('Input harus berupa angka bilangan bulat!')
+            print("Input harus berupa angka bilangan bulat!")
+
 
 def checkValue(value):
     if value >= 90:
-        return 'A+'
+        return "A+"
     elif value >= 80:
-        return 'A-'
+        return "A-"
     elif value >= 75:
-        return 'B+'
+        return "B+"
     elif value >= 70:
-        return 'B-'
+        return "B-"
     elif value >= 65:
-        return 'C+'
+        return "C+"
     elif value >= 60:
-        return 'C-'
+        return "C-"
     elif value >= 50:
-        return 'D'
+        return "D"
     else:
-        return 'E'
+        return "E"
 
-while retry != 'n':
+
+while retry != "n":
     try:
         x = getUserInput()
         print(checkValue(x))
-        retry = input('Ingin mengulangi program? (y/n): ')
-        os.system('cls')
+        retry = input("Ingin mengulangi program? (y/n): ")
+        os.system("cls")
     except KeyboardInterrupt:
-        print('Anda telah membatalkan input.')
-    
-print('\nProgram Selesai!')
+        print("Anda telah membatalkan input.")
+
+print("\nProgram Selesai!")
