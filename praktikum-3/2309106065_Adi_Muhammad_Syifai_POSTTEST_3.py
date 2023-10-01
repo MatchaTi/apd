bbe6a4d5ba6432448f4f57dcc9343697680cd4d1
MathcaTi <tehmatcha28@gmail.com>
2023-10-02 07:34:32 +0800

    feat : finish posttest3 program python

---
+++ praktikum-3/2309106065_Adi_Muhammad_Syifai_POSTTEST_3.py
@@ -0,0 +1,39 @@
+import os
+
+retry = "y"
+
+os.system("cls")
+
+print("Nama\t: Adi Muhammad Syifai")
+print("NIM\t: 2309106065")
+print("Kelas\t: Informatika B23")
+print("Post Test 3 Praktikum\n")
+
+
+def checkLeapYear():
+    while True:
+        try:
+            userInput = int(input("Masukkan tahun\t: "))
+
+            if userInput < 0:
+                raise Exception("Angka tahun tidak boleh minus!")
+
+            if (userInput % 4 == 0 and userInput % 100 != 0) or userInput % 400 == 0:
+                return f"{userInput} adalah tahun kabisat"
+
+            return f"{userInput} bukan tahun kabisat"
+
+        except ValueError:
+            os.system("cls")
+            print("Inputan harus berupa angka bilangan bulat (tahun)!")
+        except Exception as e:
+            os.system("cls")
+            print(e)
+
+
+while retry != "n":
+    print(checkLeapYear())
+    retry = input("Ingin mengulang program? (y/n)\t: ").lower()
+    os.system("cls")
+
+print("Program Selesai!")
